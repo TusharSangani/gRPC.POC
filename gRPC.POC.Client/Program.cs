5bf67336f26daf6d7873b99a38beebf0c63d03e9
Tushar Sangani <sangani.tushar@gmail.com>
2022-09-11 01:40:38 +0530

    Add project files.

---
+++ gRPC.POC.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using gRPC.POC.Service;
+using Grpc.Net.Client;
+using static gRPC.POC.Service.MyStream;
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start message streaming test");
+using var channel = GrpcChannel.ForAddress("https://localhost:7052");
+var client = new MyStreamClient(channel);
+
+ByteString randomData;
+
+using var streamingCall = client.MystreamOperation();
+
+Console.WriteLine("Sending: StartRequest, BlockName: Foo ");
+var startRequest = new MyStreamOperationRequest() { Startrequest = new StartRequest() { Blockname = "Foo" } };
+await streamingCall.RequestStream.WriteAsync(startRequest);
+for (int i = 0; i < 4; i++)
+{
+    var guidData = Guid.NewGuid().ToString();
+    randomData = ByteString.CopyFrom(guidData, System.Text.Encoding.Unicode);
+    Console.WriteLine("Sending: Chunk, Payload ..." + guidData);
+
+    var chunkRequest = new MyStreamOperationRequest() { Chunk = new Chunk() { Messagedata = randomData } };
+    await streamingCall.RequestStream.WriteAsync(chunkRequest);
+}
+await streamingCall.RequestStream.CompleteAsync();
+var result = await streamingCall.ResponseAsync;
+Console.WriteLine("MyStreamOperationResponse");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ gRPC.POC.Service/Services/StreamService.cs
@@ -0,0 +1,47 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+
+namespace gRPC.POC.Service.Services
+{
+    public class StreamService : MyStream.MyStreamBase
+    {
+        private readonly ILogger<StreamService> _logger;
+        public StreamService(ILogger<StreamService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<MyStreamOperationResponse> MystreamOperation(IAsyncStreamReader<MyStreamOperationRequest> requestStream, ServerCallContext context)
+        {
+            try
+            {
+                while (await requestStream.MoveNext()
+                  && !context.CancellationToken.IsCancellationRequested)
+                {
+                    // read incoming message 
+                    var current = requestStream.Current;
+                    if (current.RequestCase == MyStreamOperationRequest.RequestOneofCase.Startrequest)
+                    {
+                        // TusharSangani: Haven't used string.format or  $ for combining string as it
+                        //can slow down the logging.
+                        _logger.LogInformation("Received: StartRequest, BlockName:" + requestStream.Current.Startrequest.Blockname);
+                    }
+                    else if (current.RequestCase == MyStreamOperationRequest.RequestOneofCase.Chunk)
+                    {
+                        _logger.LogInformation("Received: Chunk, Payload ..." + await (new ByteArrayContent(requestStream.Current.Chunk.Messagedata.ToByteArray())).ReadAsStringAsync());
+                    }                    
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                _logger.LogInformation("Operation Cancelled");
+            }
+
+            _logger.LogInformation("Operation Complete.");          
+
+            return new MyStreamOperationResponse();
+        }
+
+       
+    }
+}
